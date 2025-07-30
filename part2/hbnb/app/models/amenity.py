226da59cdc71395026ec3a079642db261b949f9b
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-07-30 12:29:13 +0200

    Added the Amenity class for managing amenities, with attribute validation during initialization.

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,1 +1,14 @@
- 
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Classe pour la gestion des équipements"""
+    def __init__(self, name):
+        super().__init__()
+
+        # Validation des attributs
+        if not name or len(name) > 50:
+            raise ValueError("The name of the equipment is required and must be less than 50 characters.")
+        
+        # Initialisation des attributs de l'objet
+        self.name = name
