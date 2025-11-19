cea56ec41fe5205e37d5f9b89d1044a073b48a4b
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-11-19 10:54:26 +0100

    Add JWT authentication support by integrating flask-jwt-extended into the application
    Initializes the JWTManager and configures the JWT_SECRET_KEY using the existing SECRET_KEY. Additionally, it registers the authentication namespace in the API, enhancing the application's security features.

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,24 +1,32 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(repositories=None, config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY')
+
     bcrypt.init_app(app)
+    jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Enregistrement des namespaces
+    from app.api.v1.auth import api as auth_ns
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     # Stocker les repositories dans l'application
     if repositories:
